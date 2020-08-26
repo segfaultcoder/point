9d3a014fe56426e5f91076df201612f9fa42ecbb
segfaultcoder <segfaultycode@gmail.com>
2020-08-26 15:15:57 +0200

    Added license.

--- point.h
+++ point.h
@@ -1,6 +1,5 @@
 /*
  * point engine: An easy to use, 2D/3D, GPU-accelerated engine based on SFML.
- * Made by segfaultcoder.
  * 
  * Last updated on: 08/26/2020
  * 
@@ -11,6 +10,20 @@
  *  - ( ) Android
  *  - ( ) Nintendo Switch
  *  - ( ) PS Vita
+ * 
+ * Copyright 2020 segfaultcoder
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
 */
 
 #ifndef __POINT
